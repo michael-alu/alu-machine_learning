dc136a8e6849c5673d01a3f3e74332e7f27b9f36
Michael <m.nwuju@alustudent.com>
2026-05-22 13:14:33 +0200

    fix: vae_loss incorrect divergence calculation

--- unsupervised_learning/autoencoders/3-variational.py
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -63,10 +63,13 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
         """variational autoencoder loss function"""
 
         xent_loss = keras.backend.binary_crossentropy(x, x_decoded_mean)
-        xent_loss = keras.backend.sum(xent_loss, axis=1)
-        kl_loss = - 0.5 * keras.backend.mean(
+        xent_loss = keras.backend.sum(xent_loss, axis=-1)
+        
+        # Change .mean to .sum here
+        kl_loss = - 0.5 * keras.backend.sum(
             1 + z_log_sigma - keras.backend.square(z_mean) - keras.backend.exp(
                 z_log_sigma), axis=-1)
+        
         return xent_loss + kl_loss
 
     auto.compile(optimizer='adam', loss=vae_loss)
